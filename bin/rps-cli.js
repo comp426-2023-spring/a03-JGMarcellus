f0af630626a85a8b8c8f5328ccdb9637e0b0acbf
JGMarcellus <jmarcellus@unc.edu>
2023-03-02 17:23:39 -0500

    Error and help messages

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -6,6 +6,10 @@ import minimist from 'minimist';
 
 const args = minimist(process. argv.slice(2));
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 if (args.h || args.help) {
 	console.log(`Usage: node-rps [SHOT]
 Play Rock Paper Scissors (RPS)
@@ -18,4 +22,24 @@ Examples:
 		  e.g. {"player":"rock"}
   node-rps rock   Return JSON with results for RPS played against a simulated opponent.
 		  e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+}
+
+if (args.r || args.rules) {
+	console.log(`Rules for Rock Paper Scissors:
+
+  - Scissors CUTS Paper
+  - Paper COVERS Rock
+  - Rock CRUSHES Scissors`);
+}
+
+const valid = ["rock", "paper", "scissors"];
+
+if (args._.length == 0) {
+	var result = rps();
+}
+else if (args._.length == 1 && valid.includes(args._[0].toLowerCase())) {
+	var result = rps(args._[0].toLowerCase());
+}
+else {
+	console.error(args._[0] + " is out of range.");
 }
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,3 +1,49 @@
 #!/usr/bin/env node
 
 import { rpsls } from '../lib/rpsls.js';
+
+import minimist from 'minimist';
+
+const args = minimist(process. argv.slice(2));
+
+if (args.h || args.help) {
+	console.log(`Usage: node-rpsls [SHOT]
+Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+
+  -h, --help        display this help message and exit
+  -r, --rules       display the rules and exit
+
+Examples:
+  node-rpsls        Return JSON with single player RPSLS result.
+                    e.g. {"player":"rock"}
+  node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"Spock","result":"lose"}`);
+}
+
+if (args.r || args.rules) {
+	console.log(`Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
+
+  - Scissors CUTS Paper
+  - Paper COVERS Rock
+  - Rock SMOOSHES Lizard
+  - Lizard POISONS Spock
+  - Spock SMASHES Scissors
+  - Scissors DECAPITATES Lizard
+  - Lizard EATS Paper
+  - Paper DISPROVES Spock
+  - Spock VAPORIZES Rock
+  - Rock CRUSHES Scissors`);
+}
+
+const valid = ["rock", "paper", "scissors", "lizard", "spock"];
+
+if (args._.length == 0) {
+	var result = rps();
+}
+
+else if (args._.length == 1 && valid.includes(args._[0].toLowerCase())) {
+	var result = rps(args._[0].toLowerCase());
+}
+else {
+	console.error(args._[0] + " is out of range.");
+}
